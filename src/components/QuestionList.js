7ca9533f0452cff6f5aa3da435d2d56511c7bf3e
aaaqil <aaaqil.west@student.moringaschool.com>
2025-07-28 15:03:22 +0300

    Implement full CRUD for quiz admin (fetch, add, edit, delete questions)

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,39 @@
-import React from "react";
 
-function QuestionList() {
+import QuestionItem from "./QuestionItem";
+
+function QuestionList({ questions, setQuestions }) {
+  function handleDelete(id) {
+    fetch(`http://localhost:4000/questions/${id}`, { method: "DELETE" })
+      .then(() => {
+        setQuestions((prev) => prev.filter((q) => q.id !== id));
+      });
+  }
+
+  function handleUpdate(id, correctIndex) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ correctIndex }),
+    })
+      .then((r) => r.json())
+      .then((updated) => {
+        setQuestions((prev) => prev.map((q) => (q.id === id ? updated : q)));
+      });
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((q) => (
+          <QuestionItem
+            key={q.id}
+            question={q}
+            onDelete={handleDelete}
+            onUpdate={handleUpdate}
+          />
+        ))}
+      </ul>
     </section>
   );
 }
